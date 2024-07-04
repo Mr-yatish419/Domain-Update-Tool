432960503ffec48a0595793d27b0dc13170b1390
yatish rivankar <yatishrivankar336@gmail.com>
2024-07-04 16:59:28 +0530

    chore: message

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({ 
-  transpileDependencies: true
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080, // or the port your application is using
+    allowedHosts: 'all',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    https: true,
+    client: {
+      webSocketURL: 'wss://c4c9-115-113-30-230.ngrok-free.app/ws',
+    },
   }
-);
+});
 
 
 
